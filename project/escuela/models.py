3336d73cf18c407cdfec9c9bfb850803e113d276
AlePezzelato <apezzelato1@gmail.com>
2024-05-13 18:33:13 -0300

    cree las bases de datos

---
+++ project/escuela/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=150, unique=True)
+    codigo = models.CharField(max_length=30, unique=True)
+
+class Docente(models.Model):
+    nombre = models.CharField(max_length=150)
+    apellido = models.CharField(max_length=150)
+    materia_id = models.ForeignKey(Materia, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=150)
+    apellido = models.CharField(max_length=150)
+    fecha_de_nacimiento = models.DateField(null=True, blank=True)
+    docente_id = models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True)
+    materia_id = models.ForeignKey(Materia, on_delete=models.SET_NULL, null=True)
+    
+    
