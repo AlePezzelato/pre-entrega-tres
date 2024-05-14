fce151fdab41dade1dfecc38f810641481d9f569
AlePezzelato <apezzelato1@gmail.com>
2024-05-14 19:33:57 -0300

    agregue eliminar y modificar

--- project/escuela/urls.py
+++ project/escuela/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path
-from escuela.views import index, listar_alumnos, crear_alumno
+from escuela.views import index, listar_alumnos, crear_alumno, modificar_alumno, eliminar_alumno
 
 app_name = "escuela"
 
 urlpatterns = [
     path("", index, name="index"),
     path("escuela/listar_alumnos", listar_alumnos, name="listar_alumnos"),
-    path("escuela/crear alumno", crear_alumno, name="crear_alumno"),
+    path("escuela/crear_alumno", crear_alumno, name="crear_alumno"),
+    path("escuela/modificar_alumno/<int:pk>", modificar_alumno, name="modificar_alumno"),
+    path("escuela/eliminar_alumno/<int:pk>", eliminar_alumno, name="eliminar_alumno"),
 ]
--- project/escuela/views.py
+++ project/escuela/views.py
@@ -20,3 +20,21 @@ def crear_alumno(request):
         form = AlumnoForm()
     return render(request, "escuela/crear_alumno.html",{"form":form})
 
+def modificar_alumno(request, pk:int):
+    consulta =modificar_alumno.objects.get(id=pk)
+    if request.method == "POST":
+        form = AlumnoForm(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            return redirect("escuela:listar_alumnos")
+    else:
+        form = AlumnoForm()
+    return render(request, "escuela/modificar_alumno.html",{"form":form})
+
+def eliminar_alumno(request, pk:int):
+    consulta =eliminar_alumno.objects.get(id=pk)
+    if request.method == "POST":
+        consulta.delete()
+        return redirect("escuela:listar_alumnos")
+    return render (request, "escuela/confirma_eliminacion_alumno.html", {"object":consulta})
+    
