ac3db0cf7fbfe3ec60fe0f8067f90ab65c89a312
AlePezzelato <apezzelato1@gmail.com>
2024-05-14 11:17:58 -0300

    modifique app escuela y cree consulta a la base de alumnos

--- project/escuela/urls.py
+++ project/escuela/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from escuela.views import index
+from escuela.views import index, listar_alumnos
 
 app_name = "escuela"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("escuela/listar_alumnos", listar_alumnos, name="listar_alumnos")
 ]
--- project/escuela/views.py
+++ project/escuela/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from escuela.models import Alumno
 
 def index(request):
     return render (request, "escuela/index.html")
+
+def listar_alumnos(request):
+    consulta = Alumno.objects.all()
+    contexto = {"Alumnos":consulta}
+    return render(request, "escuela/listar_alumnos.html", contexto)
