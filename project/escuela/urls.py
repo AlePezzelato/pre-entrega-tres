f690e12507518ef051f1b67e49d20a12ff328ed2
AlePezzelato <apezzelato1@gmail.com>
2024-05-14 19:56:20 -0300

    elimine la opcion modificar y eliminar alumno porque se rompia todo y no le encontre la vuelta

--- project/escuela/urls.py
+++ project/escuela/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from escuela.views import index, listar_alumnos, crear_alumno, modificar_alumno, eliminar_alumno
+from escuela.views import index, listar_alumnos, crear_alumno
 
 app_name = "escuela"
 
@@ -8,6 +8,4 @@ urlpatterns = [
     path("", index, name="index"),
     path("escuela/listar_alumnos", listar_alumnos, name="listar_alumnos"),
     path("escuela/crear_alumno", crear_alumno, name="crear_alumno"),
-    path("escuela/modificar_alumno/<int:pk>", modificar_alumno, name="modificar_alumno"),
-    path("escuela/eliminar_alumno/<int:pk>", eliminar_alumno, name="eliminar_alumno"),
 ]
